84bb95cc9e059d7334805b5933cd4035f911a933
EndruBul <109689781+EndruBul@users.noreply.github.com>
2022-07-20 21:16:30 +0300

    Update and rename config/myconfig_template.py to конфигурация/myconfig_template.py

--- конфигурация/myconfig_template.py
+++ конфигурация/myconfig_template.py
@@ -1,3 +1,5 @@
 # Update the API key and the API secret in this file and rename it to myconfig.py
 API_KEY = '<UPDATE_API_KEY>' 
 API_SECRET = '<UPDATE_API_SECRET>'
+lQepoZfybR7ziwdINHXuQjrY9hZkD8IKBiKIyqOIhV5g8zJ2rGzaxJR7rTF5W0Ij
+0icppHyMbBBcLaZJWtWmCcOtncoG9h5SHmVpFdusQEAqA1sumqd39WQB617Wo3Ql
