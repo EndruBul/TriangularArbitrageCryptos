94225e9ddfc30cac4c6bd5d7da7157aa5a0246f9
EndruBul <109689781+EndruBul@users.noreply.github.com>
2022-07-20 21:20:08 +0300

    Update and rename config/myconfig_template.py to конфигурация/myconfig_template.py
    MoyBot

---
+++ конфигурация/myconfig_template.py
@@ -0,0 +1,4 @@
+# Update the API key and the API secret in this file and rename it to myconfig.py
+API_KEY = <UPDATE_lQepoZfybR7ziwdINHXuQjrY9hZkD8IKBiKIyqOIhV5g8zJ2rGzaxJR7rTF5W0IjAPI_KEY>'
+API_SECRET = '<UPDATE_API_SECRET>'
+0icppHyMbBBcLaZJWtWmCcOtncoG9h5SHmVpFdusQEAqA1sumqd39WQB617Wo3Ql
